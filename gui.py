b57e00759c39fd08555c03254ced8b1ce194bea5
zengtaotao3390 <378041351@qq.com>
2019-04-24 11:59:24 +0800

    add get entry

--- gui.py
+++ gui.py
@@ -2,6 +2,7 @@
 from tkinter.filedialog import askopenfilename
 from tkinter import *
 import tkinter as tk
+import SplitAndCat
 
 
 def get_filename():
@@ -10,12 +11,16 @@ def get_filename():
     text_fragment_file.delete(1.0, END)
     text_fragment_file.insert(END, filename)
 
-def fragMent
+
+def split_file():
+    size = int(entry_fragment_size.get())
+    SplitAndCat.split_file()
+
+
 
 r = Tk()
 r.geometry('{}x{}'.format(460, 350))
-
-label_fragment_size = Label(r, text='请输入要分割的文件大小单位G：')
+label_fragment_size = Label(r, text='请输入要分割的文件大小单位M：')
 label_fragment_size.grid(row=0)
 entry_fragment_size = Entry(r)
 entry_fragment_size.grid(row=0, column=1)
@@ -23,8 +28,8 @@ text_fragment_file = Text(r, height=5, width=30)
 text_fragment_file.grid(row=1, column=0)
 button_select_file = tk.Button(r, text='选择文件', width=10, command=get_filename, fg="red")
 button_select_file.grid(row=1, column=1)
-button_start = tk.Button(r, text='开始分割', width=10, command=fragment_file, fg="red")
-
+button_start = tk.Button(r, text='开始分割', width=10, command=split_file, fg="red")
+button_start.grid(row=2, column=1)
 
 r.mainloop()
 
