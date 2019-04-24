fecda550b9ed1ca05ba4d76a0a28a01d04b36d12
zengtaotao3390 <378041351@qq.com>
2019-04-24 11:11:52 +0800

    Initial commit

---
+++ gui.py
@@ -0,0 +1,30 @@
+
+from tkinter.filedialog import askopenfilename
+from tkinter import *
+import tkinter as tk
+
+# filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
+# print(filename)
+
+
+def get_filename():
+    filename = askopenfilename()
+    print(filename)
+    text_fragment_file.delete(1.0, END)
+    text_fragment_file.insert(END, filename)
+
+
+r = Tk()
+r.geometry('{}x{}'.format(460, 350))
+
+label_fragment_size = Label(r, text='请输入要分割的文件大小单位G：')
+label_fragment_size.grid(row=0)
+entry_fragment_size = Entry(r)
+entry_fragment_size.grid(row=0, column=1)
+text_fragment_file = Text(r, height=5, width=30)
+text_fragment_file.grid(row=1, column=0)
+button = tk.Button(r, text='选择文件', width=10, command=get_filename, fg="red").grid(row=1, column=1)
+
+r.mainloop()
+
+
